335220b96a188fb6fa3a5a6239166d07fedd1366
Piotr Romanowski <piotr@romanowski.me>
2022-06-10 18:53:04 +0200

    Add behaviour grade endpoint

---
+++ Vulcanova.Uonet/Api/Grades/GetBehaviourGradeByPupilQuery.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Vulcanova.Uonet.Api.Grades
+{
+    public record GetBehaviourGradeByPupilQuery(
+        int UnitId,
+        int PupilId,
+        int PeriodId,
+        DateTime LastSyncDate,
+        int PageSize,
+        int LastId = int.MinValue) : IPaginatedApiQuery<GradePayload>
+    {
+        public IPaginatedApiQuery<GradePayload> NextPage(int lastId)
+            => this with {LastId = lastId};
+
+        public const string ApiEndpoint = "mobile/grade/behaviour/byPupil";
+    }
+}
