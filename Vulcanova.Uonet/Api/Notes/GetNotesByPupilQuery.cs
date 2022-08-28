4d6fca9d8441388a802639227c8b263ae5ecc2d9
MRmlik12 <mrmlik12sup@gmail.com>
2022-08-28 17:53:30 +0200

    Reformat Notes dir

--- Vulcanova.Uonet/Api/Notes/GetNotesByPupilQuery.cs
+++ Vulcanova.Uonet/Api/Notes/GetNotesByPupilQuery.cs
@@ -8,10 +8,11 @@ namespace Vulcanova.Uonet.Api.Notes
         int LastId = int.MinValue,
         int PageSize = 500) : IPaginatedApiQuery<NotesPayload>
     {
-        public IPaginatedApiQuery<NotesPayload> NextPage(int lastId)
-            => this with { LastId = lastId };
-
         public const string ApiEndpoint = "mobile/note/byPupil";
-        
+
+        public IPaginatedApiQuery<NotesPayload> NextPage(int lastId)
+        {
+            return this with { LastId = lastId };
+        }
     }
 }
